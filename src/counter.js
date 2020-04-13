60d7a672bac2c019ff41ea0b1128f63fcfee2663
Adela Darmansyah <adela.yohana@gmail.com>
2020-04-13 11:38:58 -0700

    Attempt to create a recursive function

--- src/counter.js
+++ src/counter.js
@@ -1,5 +1,35 @@
+const QUARTER = 0.25;
+const DIME = 0.10;
+const NICKEL = 0.05;
+const PENNY = 0.01;
+
 export class Counter {
+
   constructor(input) {
     this.userInput = parseFloat(input.toFixed(2));
+    this.quarters;
+    this.dimes;
+    this.nickels;
+    this.pennies;
   }
+
+  calculateChange = (input) => {
+    if (isNaN(input)) {
+      return;
+    }
+
+    let change;
+
+    if (input >= QUARTER) {
+      this.quarters = Math.floor(input / QUARTER);
+      change = parseFloat((input % QUARTER).toFixed(2));
+      console.log(">>>>> Q =" + this.quarters);
+    } else if (input >= DIME) {
+      this.dimes = Math.floor(input / DIME);
+      change = parseFloat((input % DIME).toFixed(2));
+      console.log(">>>>> D =" + this.dimes);
+    }
+    return;
+  }
+
 }
